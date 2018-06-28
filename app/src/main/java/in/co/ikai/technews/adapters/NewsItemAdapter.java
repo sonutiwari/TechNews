aefbc32abe21c142859b397495a69caf2527026d
sonutiwari <ankorha@gmail.com>
2018-06-28 15:25:02 +0530

    Fixed Picture not loading bug

--- app/src/main/java/in/co/ikai/technews/adapters/NewsItemAdapter.java
+++ app/src/main/java/in/co/ikai/technews/adapters/NewsItemAdapter.java
@@ -3,7 +3,6 @@ package in.co.ikai.technews.adapters;
 import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
